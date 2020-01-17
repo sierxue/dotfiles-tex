e13f31dab614a19ae74470b186c459bcdb2cbc3d
sierxue <19348230+sierxue@users.noreply.github.com>
2020-01-18 00:23:48 +0800

    alias: Set aliases for using texliveonfly command to compile.

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -2,3 +2,9 @@
 
 alias a='git gui' # This alias has been defined in zsh git plugin.
 alias amsp="git submodule update --remote --recursive && git add --all && git commit -m 'Update submodules' && git push"
+
+# Aliases related to latex:
+
+alias tl="python3 /usr/local/texlive/2019/texmf-dist/scripts/texliveonfly/texliveonfly.py --compiler=lualatex"
+alias tp="python3 /usr/local/texlive/2019/texmf-dist/scripts/texliveonfly/texliveonfly.py --compiler=pdflatex"
+alias tx="python3 /usr/local/texlive/2019/texmf-dist/scripts/texliveonfly/texliveonfly.py --compiler=xelatex"
