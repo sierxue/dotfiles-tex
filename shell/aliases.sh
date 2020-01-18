396805f294ea583539c6e6f6f87fc78d4c1fe089
sierxue <19348230+sierxue@users.noreply.github.com>
2020-01-18 12:44:12 +0800

    Update submodules

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -1,7 +1,12 @@
 # Aliases related to git:
 
 alias a='git gui' # This alias has been defined in zsh git plugin.
-alias amsp="git submodule update --remote --recursive && git add --all && git commit -m 'Update submodules' && git push"
+alias amsp="
+git submodule update --remote --recursive
+git add --all
+git commit -m 'Update submodules'
+git push
+"
 
 # Aliases related to latex:
 
